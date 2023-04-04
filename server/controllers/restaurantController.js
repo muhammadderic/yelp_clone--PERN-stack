8312a8efcbf5a68b6520d846763376381189c817
muhammadderic <muhammad.donny.e@gmail.com>
2023-04-04 15:45:19 +0700

    Create query for create new restaurant

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -1,28 +1,54 @@
 const db = require("../db/db");
 
 // CREATE DATA
-const createRestaurant = (req, res) => {
-  res.send("Create Restaurant");
+const createRestaurant = async (req, res) => {
+  try {
+    const { name, location, price_range } = req.body;
+    const results = await db.query(
+      "INSERT INTO restaurants(name, location, price_range) VALUES($1, $2, $3) RETURNING *", [name, location, price_range]
+    );
+    res.status(201).json({
+      restaurant: results.rows[0]
+    })
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 // GET ALL DATA
-const getAllRestaurants = (req, res) => {
-  res.send("Get All Restaurants");
+const getAllRestaurants = async (req, res) => {
+  try {
+    res.send("Get All Restaurants");
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 // GET SINGLE DATA
-const getRestaurant = (req, res) => {
-  res.send("Get Restaurant");
+const getRestaurant = async (req, res) => {
+  try {
+    res.send("Get Restaurant");
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 // UPDATE DATA
-const updateRestaurant = (req, res) => {
-  res.send("Update Restaurant");
+const updateRestaurant = async (req, res) => {
+  try {
+    res.send("Update Restaurant");
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 // DELETE DATA
-const deleteRestaurant = (req, res) => {
-  res.send("Delete Restaurant");
+const deleteRestaurant = async (req, res) => {
+  try {
+    res.send("Delete Restaurant");
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 module.exports = {
--- server/db/db.sql
+++ server/db/db.sql
@@ -6,3 +6,6 @@ CREATE TABLE restaurants (
   location VARCHAR(50) NOT NULL,
   price_range INT NOT NULL CHECK(price_range > 0 AND price_range <= 5)
 );
+
+-- Create New Restaurant
+INSERT INTO restaurants(name, location, price_range) VALUES('mcDonald', 'New York', 3)
