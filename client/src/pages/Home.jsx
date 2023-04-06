2227af9221c1481fd56a5a70b336c50c63820ec0
muhammadderic <muhammad.donny.e@gmail.com>
2023-04-06 11:34:56 +0700

    Show restaurants data in Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,23 @@
-import { Container, Flex, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { Button, Container, Flex, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Home() {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await fetch("http://localhost:5000/api/v1/")
+          .then(res => res.json())
+          .then(data => setRestaurants(data.restaurants));
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <Container maxW='container.md'>
       <Flex justify='center'>
@@ -19,14 +36,24 @@ function Home() {
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
-                <Td>Restaurant</Td>
-                <Td>City</Td>
-                <Td isNumeric>2</Td>
-                <Td >Good</Td>
-                <Td >Edit</Td>
-                <Td >Delete</Td>
-              </Tr>
+              {restaurants.map(restaurant => (
+                <Tr key={restaurant.id}>
+                  <Td>{restaurant.name}</Td>
+                  <Td>{restaurant.location}</Td>
+                  <Td isNumeric>{restaurant.price_range}</Td>
+                  <Td >Good</Td>
+                  <Td >
+                    <Button variant="solid">
+                      Edit
+                    </Button>
+                  </Td>
+                  <Td >
+                    <Button variant="solid">
+                      Delete
+                    </Button>
+                  </Td>
+                </Tr>
+              ))}
             </Tbody>
           </Table>
         </TableContainer>
